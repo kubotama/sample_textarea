eb76c7b63f22913f352f4122e84ddc96b49024fb
kubotama <16697573+kubotama@users.noreply.github.com>
2020-04-01 23:33:06 +0900

    コンポーネントのマウントを共通ルーチンとして外だし

--- tests/unit/textarea.spec.js
+++ tests/unit/textarea.spec.js
@@ -2,13 +2,15 @@ import { shallowMount } from "@vue/test-utils";
 import SampleTextArea from "@/components/SampleTextArea.vue";
 
 describe("SampleTextAreaコンポーネント", () => {
+  let wrapper;
+  beforeEach(() => {
+    wrapper = shallowMount(SampleTextArea);
+  });
   it("vueコンポーネントである。", () => {
-    const wrapper = shallowMount(SampleTextArea);
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 
   it("コンポーネント上に要素が存在しない。", () => {
-    const wrapper = shallowMount(SampleTextArea);
     expect(wrapper.isEmpty()).toBeTruthy();
   });
 });
