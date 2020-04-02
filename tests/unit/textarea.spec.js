509fe37ad4721f29b5032cf4a8161bb7afd760f1
kubotama <16697573+kubotama@users.noreply.github.com>
2020-04-02 22:14:58 +0900

    リファクタリング
    textareaのelementの変数への代入をbeforeEachで共有

--- tests/unit/textarea.spec.js
+++ tests/unit/textarea.spec.js
@@ -2,28 +2,29 @@ import { shallowMount } from "@vue/test-utils";
 import SampleTextArea from "@/components/SampleTextArea.vue";
 
 describe("SampleTextAreaコンポーネント", () => {
+  const idTa = "#sampleTextArea";
   let wrapper;
+  let elTa;
+
   beforeEach(() => {
     wrapper = shallowMount(SampleTextArea);
+    elTa = wrapper.find(idTa).element;
   });
   it("vueコンポーネントである。", () => {
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 
-  const idTa = "#sampleTextArea";
   it("textareaの存在をテストする。", () => {
     expect(wrapper.find(idTa).exists()).toBeTruthy();
   });
 
   describe("textareaの初期設定を確認する。", () => {
     it('初期値は""とする。', () => {
-      expect(wrapper.find(idTa).element.value).toBe("");
+      expect(elTa.value).toBe("");
     });
 
     it("textareaのplaceholderは「変換したい文字列を入力してください」とする。", () => {
-      expect(wrapper.find(idTa).element.placeholder).toBe(
-        "変換したい文字列を入力してください"
-      );
+      expect(elTa.placeholder).toBe("変換したい文字列を入力してください");
     });
   });
 });
