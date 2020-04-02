79b664cbca763a46d09feb1089a96bbb14927799
kubotama <16697573+kubotama@users.noreply.github.com>
2020-04-02 11:02:25 +0900

    textareaを追加したため、コンポーネント上が空白のテスをと削除

--- tests/unit/textarea.spec.js
+++ tests/unit/textarea.spec.js
@@ -10,10 +10,6 @@ describe("SampleTextAreaコンポーネント", () => {
     expect(wrapper.isVueInstance()).toBeTruthy();
   });
 
-  it.skip("コンポーネント上に要素が存在しない。", () => {
-    expect(wrapper.isEmpty()).toBeTruthy();
-  });
-
   it("textareaの存在をテストする。", () => {
     expect(wrapper.find("#sampleTextArea").exists()).toBeTruthy();
   });
