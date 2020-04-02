1bb8b62a9addccb26c25c5314dd5519777cd39f3
kubotama <16697573+kubotama@users.noreply.github.com>
2020-04-02 11:21:46 +0900

    初期値を確認するテストを追加した。

--- tests/unit/textarea.spec.js
+++ tests/unit/textarea.spec.js
@@ -13,4 +13,10 @@ describe("SampleTextAreaコンポーネント", () => {
   it("textareaの存在をテストする。", () => {
     expect(wrapper.find("#sampleTextArea").exists()).toBeTruthy();
   });
+
+  describe("textareaの初期設定を確認する。", () => {
+    it('初期値は""とする。', () => {
+      expect(wrapper.find("#sampleTextArea").element.value).toBe("");
+    });
+  });
 });
