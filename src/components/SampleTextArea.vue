9cbf81dbccc60a8701de20d687febbae9dacd29d
kubotama <16697573+kubotama@users.noreply.github.com>
2020-05-25 15:40:22 +0900

    プライバシーポリシーの本文やボタンなどを修正した

--- src/components/SampleTextArea.vue
+++ src/components/SampleTextArea.vue
@@ -12,23 +12,26 @@
     <v-dialog v-model="dialog" width="500">
       <template v-slot:activator="{ on }">
         <v-btn color="red lighten-2" dark v-on="on">
-          Click Me
+          プライバシーポリシー
         </v-btn>
       </template>
 
       <v-card>
         <v-card-title class="headline grey lighten-2" primary-title>
-          Privacy Policy
+          プライバシーポリシー
         </v-card-title>
 
         <v-card-text>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-          aliquip ex ea commodo consequat. Duis aute irure dolor in
-          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-          culpa qui officia deserunt mollit anim id est laborum.
+          当サイトでは、Googleによるアクセス解析ツール「Googleアナリティクス」を使用しています。このGoogleアナリティクスはデータの収集のためにCookieを使用しています。このデータは匿名で収集されており、個人を特定するものではありません。
+          この機能はCookieを無効にすることで収集を拒否することが出来ますので、お使いのブラウザの設定をご確認ください。
+          この規約に関しての詳細は<a
+            href="https://marketingplatform.google.com/about/analytics/terms/jp/"
+          >
+            Googleアナリティクスサービス利用規約のページ </a
+          >や
+          <a href="https://policies.google.com/technologies/ads?hl=ja"
+            >Googleポリシーと規約ページ</a
+          >をご覧ください。
         </v-card-text>
 
         <v-divider></v-divider>
@@ -36,7 +39,7 @@
         <v-card-actions>
           <v-spacer></v-spacer>
           <v-btn color="primary" text @click="dialog = false">
-            I accept
+            閉じる
           </v-btn>
         </v-card-actions>
       </v-card>
